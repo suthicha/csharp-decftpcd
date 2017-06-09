434866b7c8bef636a6b4a90af09d02b1fd675006
IAMSUTHICHA <suthicha@outlook.com>
2017-06-09 11:38:21 +0700

    update

--- DECFtpCD.WinApp/mainForm.Designer.cs
+++ DECFtpCD.WinApp/mainForm.Designer.cs
@@ -28,20 +28,248 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtPeriod = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTaxNumber = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboShipmentType = new System.Windows.Forms.ComboBox();
+            this.btnExportForCustomer = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtTotalRecord = new System.Windows.Forms.ToolStripStatusLabel();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtPeriod
+            // 
+            this.txtPeriod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtPeriod.Location = new System.Drawing.Point(123, 45);
+            this.txtPeriod.MaxLength = 6;
+            this.txtPeriod.Name = "txtPeriod";
+            this.txtPeriod.Size = new System.Drawing.Size(150, 22);
+            this.txtPeriod.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnSearch.Location = new System.Drawing.Point(279, 73);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Period :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(12, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tax Number :";
+            // 
+            // txtTaxNumber
+            // 
+            this.txtTaxNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtTaxNumber.Location = new System.Drawing.Point(123, 73);
+            this.txtTaxNumber.Name = "txtTaxNumber";
+            this.txtTaxNumber.Size = new System.Drawing.Size(150, 22);
+            this.txtTaxNumber.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label7.Location = new System.Drawing.Point(7, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(863, 2);
+            this.label7.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Size = new System.Drawing.Size(870, 403);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.progressBar1,
+            this.toolStripStatusLabel1,
+            this.txtTotalRecord});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 517);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(884, 24);
+            this.statusStrip1.TabIndex = 14;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.lblStatus.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(43, 19);
+            this.lblStatus.Text = "Status";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 18);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(12, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Shipment : ";
+            // 
+            // cboShipmentType
+            // 
+            this.cboShipmentType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboShipmentType.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cboShipmentType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.cboShipmentType.FormattingEnabled = true;
+            this.cboShipmentType.Items.AddRange(new object[] {
+            "",
+            "EXPORT",
+            "IMPORT"});
+            this.cboShipmentType.Location = new System.Drawing.Point(123, 15);
+            this.cboShipmentType.Name = "cboShipmentType";
+            this.cboShipmentType.Size = new System.Drawing.Size(150, 24);
+            this.cboShipmentType.TabIndex = 0;
+            // 
+            // btnExportForCustomer
+            // 
+            this.btnExportForCustomer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExportForCustomer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btnExportForCustomer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExportForCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnExportForCustomer.ForeColor = System.Drawing.Color.White;
+            this.btnExportForCustomer.Location = new System.Drawing.Point(720, 69);
+            this.btnExportForCustomer.Name = "btnExportForCustomer";
+            this.btnExportForCustomer.Size = new System.Drawing.Size(150, 28);
+            this.btnExportForCustomer.TabIndex = 4;
+            this.btnExportForCustomer.Text = "EXPORT PDF";
+            this.btnExportForCustomer.UseVisualStyleBackColor = false;
+            this.btnExportForCustomer.Click += new System.EventHandler(this.btnExportForCustomer_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(684, 19);
+            this.toolStripStatusLabel1.Spring = true;
+            // 
+            // txtTotalRecord
+            // 
+            this.txtTotalRecord.BackColor = System.Drawing.SystemColors.Control;
+            this.txtTotalRecord.Name = "txtTotalRecord";
+            this.txtTotalRecord.Size = new System.Drawing.Size(40, 19);
+            this.txtTotalRecord.Text = "Count";
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(693, 400);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(221)))), ((int)(((byte)(238)))));
+            this.ClientSize = new System.Drawing.Size(884, 541);
+            this.Controls.Add(this.btnExportForCustomer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboShipmentType);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTaxNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtPeriod);
             this.Name = "mainForm";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pdf Export Helper";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPeriod;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTaxNumber;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboShipmentType;
+        private System.Windows.Forms.Button btnExportForCustomer;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel txtTotalRecord;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- DECFtpCD.WinApp/mainForm.cs
+++ DECFtpCD.WinApp/mainForm.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.FtpClient;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,9 +17,315 @@ namespace DECFtpCD.WinApp
 {
     public partial class mainForm : Form
     {
+        private int intOriginalExStyle = -1;
+        private bool bEnableAntiFlicker = true;
+        private readonly string _sqlConnection;
+        private DataSet dsResult;
+
         public mainForm()
         {
+            ToggleAntiFlicker(false);
             InitializeComponent();
+            _sqlConnection = ConfigurationManager.AppSettings["DbConnection"];
+
+            // SetDoubleBuffered(dataGridView1, true);
+            this.ResizeBegin += Form1_ResizeBegin;
+            this.ResizeEnd += Form1_ResizeEnd;
+            this.progressBar1.Style = ProgressBarStyle.Blocks;
+            initDataGridViewColumns(dataGridView1);
+
+            // txtPeriod.Text = "201701";
+            // txtTaxNumber.Text = "0105541000784";
+            txtTotalRecord.Text = "";
+        }
+
+        //public void SetDoubleBuffered(this Control control, bool setting)
+        //{
+        //    Type dgvType = control.GetType();
+        //    PropertyInfo pi = dgvType.GetProperty("DoubleBuffered",
+        //          BindingFlags.Instance | BindingFlags.NonPublic);
+        //    pi.SetValue(control, setting, null);
+        //}
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            ToggleAntiFlicker(false);
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            ToggleAntiFlicker(true);
+        }
+
+        private void ToggleAntiFlicker(bool Enable)
+        {
+            bEnableAntiFlicker = Enable;
+            this.MaximizeBox = true;
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                if (intOriginalExStyle == -1)
+                {
+                    intOriginalExStyle = base.CreateParams.ExStyle;
+                }
+                CreateParams cp = base.CreateParams;
+
+                if (bEnableAntiFlicker)
+                {
+                    cp.ExStyle |= 0x02000000; //WS_EX_COMPOSITED
+                }
+                else
+                {
+                    cp.ExStyle = intOriginalExStyle;
+                }
+
+                return cp;
+            }
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private DataGridViewTextBoxColumn createDataGridViewTextBoxColumn(
+            string title, string propertyName, int width,
+            DataGridViewContentAlignment alignment = DataGridViewContentAlignment.MiddleLeft)
+        {
+            var col = new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = propertyName,
+                HeaderText = title.ToUpper(),
+                Name = "__col__" + propertyName,
+                ReadOnly = true,
+                Width = width
+            };
+
+            col.DefaultCellStyle.Alignment = alignment;
+            return col;
+        }
+
+        private void initDataGridViewColumns(DataGridView dgv)
+        {
+            dgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dgv.EnableHeadersVisualStyles = false;
+            dgv.ColumnHeadersHeight = 28;
+            dgv.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 8.25f, FontStyle.Regular);
+            dgv.AutoGenerateColumns = false;
+            dgv.MultiSelect = false;
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.RowTemplate.Height = 24;
+            dgv.RowTemplate.MinimumHeight = 22;
+            dgv.RowHeadersVisible = false;
+            dgv.AllowUserToResizeRows = false;
+
+            //DataGridViewCheckBoxColumn chkCol = new DataGridViewCheckBoxColumn();
+            //chkCol.Name = "__col__checkbox";
+            //chkCol.HeaderText = "";
+            //chkCol.DataPropertyName = "CheckState";
+            //chkCol.Width = 50;
+            //chkCol.ReadOnly = true;
+            //chkCol.FillWeight = 10;
+            //chkCol.Resizable = DataGridViewTriState.False;
+
+            //CheckBox checkboxHeader = new CheckBox();
+            //checkboxHeader.Name = "checkboxHeader";
+            //checkboxHeader.Size = new Size(18, 18);
+            //checkboxHeader.Location = new Point(20, 5);
+            //checkboxHeader.BackColor = Color.Transparent;
+            //checkboxHeader.CheckedChanged += CheckboxHeader_CheckedChanged;
+            //dgv.Columns.Add(chkCol);
+            //dgv.Controls.Add(checkboxHeader);
+
+            dgv.Columns.Add(createDataGridViewTextBoxColumn("DecNO", "DecNO", 150, DataGridViewContentAlignment.MiddleCenter));
+            dgv.Columns.Add(createDataGridViewTextBoxColumn("Invoices", "InvNOList", 600, DataGridViewContentAlignment.MiddleCenter));
+            dgv.Columns.Add(createDataGridViewTextBoxColumn("Status", "Status", 100, DataGridViewContentAlignment.MiddleCenter));
+
+            for (int i = 0; i < dgv.Columns.Count; i++)
+            {
+                dgv.Columns[i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtPeriod.Text == "" || txtTaxNumber.Text == "" || cboShipmentType.Text == "")
+            {
+                MessageBox.Show("Please enter your period and tax number.!!!");
+                return;
+            }
+
+            if (backgroundWorker1.IsBusy) return;
+            backgroundWorker1.RunWorkerAsync(new object[] { txtPeriod.Text, txtTaxNumber.Text, cboShipmentType.Text });
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            txtTotalRecord.Text = "";
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                var parms = e.Argument as object[];
+                var period = (string)parms[0];
+                var taxNumber = (string)parms[1];
+                var shipmentType = (string)parms[2];
+                var commandText = "";
+
+                if (shipmentType == "EXPORT")
+                {
+                    commandText = @"select RefNO, DecNO, InvNOList, '' as Status from DecX_Declare where ExporterTaxNo = @taxno
+                    and Convert(Varchar(6),UDateDeclare,112) = @period
+                    and DocStatus <= 4
+                    order by DecNO asc";
+                }
+                else
+                {
+                    commandText = @"select RefNO, DecNO, InvNOList, '' as Status from DecI_Declare where ExporterTaxNo = @taxno
+                    and Convert(Varchar(6),UDateDeclare,112) = @period
+                    and DocStatus <= 4
+                    order by DecNO asc";
+                }
+
+                e.Result = getDeclarations(commandText, period, taxNumber);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Worker Error : " + ex.Message);
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            dsResult = e.Result as DataSet;
+            dataGridView1.DataSource = dsResult.Tables[0];
+            txtTotalRecord.Text = string.Format("Total {0} rec.", dataGridView1.Rows.Count);
+        }
+
+        private DataSet getDeclarations(string commandText, string period, string taxNumber)
+        {
+            DataSet ds = new DataSet();
+
+            using (SqlConnection conn = new SqlConnection(this._sqlConnection))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = commandText;
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@period", period);
+                cmd.Parameters.AddWithValue("@taxno", taxNumber);
+
+                var adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(ds);
+            }
+
+            return ds;
+        }
+
+        private void btnExportForCustomer_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Find not found data to export.");
+                return;
+            }
+
+            if (backgroundWorker2.IsBusy) return;
+
+            var dlg = folderBrowserDialog1.ShowDialog();
+
+            if (dlg == DialogResult.OK)
+            {
+                backgroundWorker2.RunWorkerAsync(new object[] {
+                folderBrowserDialog1.SelectedPath, cboShipmentType.Text });
+                progressBar1.Style = ProgressBarStyle.Marquee;
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                var parms = e.Argument as object[];
+                var path = (string)parms[0];
+                var shipment = (string)parms[1];
+                var ftpLocation = "";
+
+                if (shipment == "EXPORT")
+                    ftpLocation = ConfigurationManager.AppSettings["ftpEXPGnlPath"];
+                else
+                    ftpLocation = ConfigurationManager.AppSettings["ftpIMPGnlPath"];
+
+                for (int i = 0; i < dsResult.Tables[0].Rows.Count; i++)
+                {
+                    var dr = dsResult.Tables[0].Rows[i];
+                    var decno = dr["DecNo"].ToString();
+
+                    //download
+                    var result = DownloadPdf(path, ftpLocation, decno);
+                    dr["Status"] = result == true ? "OK" : "NOT FOUND";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Export Pdf Error : " + ex.Message);
+            }
+        }
+
+        private bool DownloadPdf(string destFolder, string location, string decno)
+        {
+            var destinationFile = Path.Combine(destFolder, decno.TrimEnd() + ".pdf");
+
+            using (var ftpClient = new FtpClient())
+            {
+                ftpClient.Host = ConfigurationManager.AppSettings["ftpHost"];
+                ftpClient.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["ftpUser"],
+                    ConfigurationManager.AppSettings["ftpPass"]);
+
+                ftpClient.Connect();
+
+                var uri = string.Format("{0}/{1}.pdf", location, decno.TrimEnd());
+
+                try
+                {
+                    using (var ftpStream = ftpClient.OpenRead(uri))
+                    using (var fileStream = File.Create(destinationFile, (int)ftpStream.Length))
+                    {
+                        var buffer = new byte[8 * 1024];
+                        int count;
+                        while ((count = ftpStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fileStream.Write(buffer, 0, count);
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Find not found " + uri);
+                }
+            }
+
+            if (File.Exists(destinationFile))
+                return true;
+            else
+                return false;
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            MessageBox.Show("Download completed.");
+
+            dataGridView1.DataSource = dsResult.Tables[0];
+            dataGridView1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DownloadPdf(@"c:\temp", @"/dmc/exp/gnl", "A0241600102443");
         }
     }
 }
